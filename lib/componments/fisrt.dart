b9e8b5f17f4338ce50d7c0d613291977219e0ea1
IPMSand <131543563+IPMSand@users.noreply.github.com>
2025-03-10 19:20:26 +0530

    Revert "Merge pull request #18 from IPMSand/Add-Event-page"
    This reverts commit 48d96f6803d4b0393c4e6b62188cb37654bb3cfc, reversing
    changes made to 0a584a14e64de6e8e8951c4e12644db2cc2769d1.

---
+++ lib/componments/fisrt.dart
@@ -0,0 +1,15 @@
+// * This folder is for reusable UI components. These are widgets that you can use in multiple screens or other widgets.
+// * **models/**:
+/*  This folder is where you store your data models. These are Dart classes that represent the data structures used by your application.
+services
+    * This folder houses service classes. These classes handle tasks like:
+        * API calls (network requests)
+        * Database interactions
+        * Any other business logic that isn't directly tied to the UI.
+* **screens/ or pages/**:
+    * This folder contains the Dart files that define the individual screens or pages of your application.
+* **widgets/**:
+    * This folder is for reusable UI components. These are widgets that you can use in multiple screens or other widgets.
+* **utils/ or helpers/**:
+    * This folder holds utility functions, constants, and other helper classes that are used throughout the applicatio
+    
