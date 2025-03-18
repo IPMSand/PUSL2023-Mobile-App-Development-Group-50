a55216d32fdf6d15812d86b81d8b7d14fff59664
IPMSand <131543563+IPMSand@users.noreply.github.com>
2025-03-18 13:50:46 +0530

    calnder.dart run ok

--- lib/screens/calander.dart
+++ lib/screens/calander.dart
@@ -1,25 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Calendar App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        scaffoldBackgroundColor: Colors.white,
-      ),
-      home: const CalendarScreen(),
-      debugShowCheckedModeBanner: false,
-    );
-  }
-}
 
 class CalendarScreen extends StatefulWidget {
   const CalendarScreen({Key? key}) : super(key: key);
