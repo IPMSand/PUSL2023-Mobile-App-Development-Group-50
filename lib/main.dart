461571a7e15401d2d869314c532bf530907de935
IPMSand <131543563+IPMSand@users.noreply.github.com>
2025-03-19 11:00:15 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'screens/home.dart';
-import 'screens/open_screen.dart';
 
 
 // keep this page as it. Don't put ur code here. just change which screen to open here
@@ -20,9 +19,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         useMaterial3: true,
       ),
-
-      home: const OpenDemo(),
-
+      home: const HomeScreen(), // first page to open in the system
+    );
   }
 }
 // ToDO: for the first login user display start 1-5 pages......
--- lib/screens/start05.dart
+++ lib/screens/start05.dart
@@ -1,6 +1,5 @@
 /*import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:mad_project/screens/home.dart';
 
 class Start05Screen extends StatelessWidget {
   const Start05Screen({super.key});
@@ -67,15 +66,11 @@ class Start05Screen extends StatelessWidget {
           children: [
             // Done Button
             ElevatedButton(
-
-               onPressed: () {
-                 Navigator.pushAndRemoveUntil(
-                     context,
-                     MaterialPageRoute(builder: (context) => HomeScreen()), // Replace NewPage() with your target page widget
-                     (Route<dynamic> route) => false,
-                 );
-            },
-
+              onPressed: () {
+                
+                debugPrint("Done button pressed");  
+                
+              },
               style: ElevatedButton.styleFrom(
                 padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 15),
                 backgroundColor: Colors.lightBlueAccent, 
