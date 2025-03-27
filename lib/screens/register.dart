9f35884c22e6567038be6017cd96e91df6b06e5f
IPMSand <131543563+IPMSand@users.noreply.github.com>
2025-03-27 11:24:11 +0530

    fixed register.dart

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -192,6 +192,42 @@ class _RegisterScreenState extends State<RegisterScreen> {
     );
   }
 
+  Widget buildFormField({
+  required String label,
+  required TextEditingController controller,
+  bool isPassword = false,
+  TextInputType keyboardType = TextInputType.text,
+  Widget? suffix,
+}) {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Text(
+        label,
+        style: const TextStyle(
+          fontSize: 16,
+          fontWeight: FontWeight.w500,
+        ),
+      ),
+      const SizedBox(height: 8),
+      TextField(
+        controller: controller,
+        obscureText: isPassword,
+        keyboardType: keyboardType,
+        decoration: InputDecoration(
+          hintText: '',
+          contentPadding: const EdgeInsets.symmetric(
+            horizontal: 20,
+            vertical: 5,
+          ),
+          suffixIcon: suffix,
+        ),
+      ),
+      const SizedBox(height: 5),
+    ],
+  );
+}
+
   Widget buildRegisterIllustration() {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
