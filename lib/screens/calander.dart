c7c648933636cead2d5ff0bc2076b6068818c728
thilinipathirana <shehali2002@gmail.com>
2025-04-01 09:35:19 +0530

    edited calendar.dart

--- lib/screens/calander.dart
+++ lib/screens/calander.dart
@@ -1,5 +1,4 @@
-//--- this code works with firebase according to the add event page---
-
+// final ver
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
