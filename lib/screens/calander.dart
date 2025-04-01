f24a34bc09b61b992d8523d3834bb9eb559f7c5b
thilinipathirana <shehali2002@gmail.com>
2025-04-01 09:33:43 +0530

    Update calander.dart

--- lib/screens/calander.dart
+++ lib/screens/calander.dart
@@ -213,4 +213,3 @@ class _CalendarScreenState extends State<CalendarScreen> {
 }
 // TODO: 'withOpacity' is deprecated and shouldn't be used. Use .withValues() to avoid precision loss.
     //Try replacing the use of the deprecated member with the replacement.
-// TODo: Bottomnav bar here is cutstom desigend, try using bottom_navbar.dart widget calling..
